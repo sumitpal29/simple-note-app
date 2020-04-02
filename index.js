aeb0e927dddd5beb17a0e605ed1a1f4bfd3fdd72
sumitpal29 <sumit@fusioncharts.com>
2020-04-02 21:57:25 +0530

    add remove feature added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const validator = require("validator");
 const chalk = require("chalk");
-const fs = require('fs');
-const yargs = require('yargs'); // const argumentVectors = process.argv;
+const yargs = require("yargs"); // const argumentVectors = process.argv;
+const notes = require("./note-app/note");
+// const fs = require('fs');
 
 // console.log('Hello world is working!!', validator.isEmail('sumit@fusioncharts.com'), argumentVectors[2])
 // const note = []
@@ -10,56 +11,66 @@ const yargs = require('yargs'); // const argumentVectors = process.argv;
 //     title: yargs.argv.title,
 //     content: ''
 // })
-const sampleData = [{
-    title: "First Note",
-    author: "sumit pal",
-    body: "Test"
-}]
-fs.writeFileSync('./note-app/note-data.json', JSON.stringify(sampleData))
-const NodeData = fs.readFileSync('./note-app/note-data.json');
-const parsedData = JSON.parse(NodeData.toString());
-// changes
-parsedData[0].title = "Sample Note - 1"
+// const sampleData = [{
+//     title: "First Note",
+//     author: "sumit pal",
+//     body: "Test"
+// }]
+// fs.writeFileSync('./note-app/note-data.json', JSON.stringify(sampleData))
+// const NodeData = fs.readFileSync('./note-app/note-data.json');
+// const parsedData = JSON.parse(NodeData.toString());
+// // changes
+// parsedData[0].title = "Sample Note - 1"
 
-console.log(parsedData);
-fs.writeFileSync('./note-app/note-data.json', JSON.stringify(parsedData))
+// console.log(parsedData);
+// fs.writeFileSync('./note-app/note-data.json', JSON.stringify(parsedData))
 
 yargs.command({
-    command: 'add',
-    describe: 'Add a note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: "string"
-        },
-        body: {
-            describe: 'Note Body',
-            demandOption: true,
-            type: "string"
-        }
+  command: "add",
+  describe: "Add a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
     },
-    handler: function (argv) {
-        console.log(`${chalk.yellow.bold(argv.title.toUpperCase())}\n${chalk.white(argv.body)}`)
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string"
     }
-})
+  },
+  handler: function(argv) {
+    notes.addNote({
+      title: argv.title,
+      body: argv.body
+    });
+  }
+});
 
 yargs.command({
-    command: 'remove',
-    describe: 'remove a note',
-    handler: function () {
-        console.log('removing a note -', chalk.red(yargs.argv.title))
-    }
-})
+  command: "notes",
+  describe: "View all notes",
+  handler: function() {
+    notes.getNote();
+  }
+});
 
 yargs.command({
-    command: 'read',
-    describe: 'read a note',
-    handler: function () {
-        console.log('reading a note -', chalk.red(yargs.argv.title))
+  command: "delete",
+  describe: "read a note",
+  builder: {
+    title: {
+      demandOption: true,
+      type: "string",
+      describe: "Add title to delete note"
     }
-})
+  },
+  handler: function(argv) {
+    notes.deleteNote(argv.title);
+  }
+});
 
 // console.log(note)
 
-yargs.parse()
+yargs.parse();
--- note-app/note.js
+++ note-app/note.js
@@ -1,7 +1,65 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const loadNotes = () => {
+  try {
+    console.log("getting notes data");
+    const databuffer = fs.readFileSync("notes.json");
+    const dataString = databuffer.toString();
+    return JSON.parse(dataString);
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
 const getNote = () => {
-    console.log('Yeah got your notes');
-}
+  console.log("Getting notes");
+  const notes = loadNotes();
+  notes.forEach(i => {
+    console.log(`\n${chalk.white(i.title)}:\t${chalk.yellow(i.body)}\n`);
+  });
+};
+
+const saveNotes = dataArr => {
+  const stringyfied = JSON.stringify(dataArr);
+  fs.writeFileSync("notes.json", stringyfied);
+};
+
+const matched = (notes, title, cb) =>
+  notes.filter((item, index) => {
+    if (item.title === title) {
+      cb && cb(index);
+      return true;
+    }
+    return false;
+  });
+
+const addNote = ({ title, body }) => {
+  const notes = loadNotes();
+  if (matched(notes, title).length === 0) {
+    notes.push({ title, body });
+    saveNotes(notes);
+  } else console.log("Title already taken", matched.length);
+};
+
+const deleteNote = title => {
+  const notes = loadNotes();
+  const filteredNotes = notes.filter(note => {
+    if (note.title === title) {
+      return false;
+    }
+    return true;
+  });
+  if (filteredNotes.length != notes.length) {
+    saveNotes(filteredNotes);
+    console.log(`note - title : ${title} , deleted!!!`);
+  } else console.log("No notes found");
+  // matched(notes, title).length ? deleteCallback : console.log('Title already taken', matched.length)
+};
 
 module.exports = {
-    getNote
-}
+  getNote,
+  addNote,
+  deleteNote
+};
